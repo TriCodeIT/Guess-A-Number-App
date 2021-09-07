83470a63260060b6684c2ef0765b10ccaa2dd9e9
TriCodeIT <bayu165kusuma@gmail.com>
2021-09-07 13:45:29 +0700

    Buildiang a Custom Button Component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -16,6 +16,7 @@ import Input from "../components/Input";
 import NumberContainer from "../components/NumberContainer";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
+import MainButton from "../components/MainButton";
 
 const StartGameScreen = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -55,7 +56,7 @@ const StartGameScreen = (props) => {
       <Card style={styles.summaryContainer}>
         <BodyText>You Selected</BodyText>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title='START GAME' onPress={() => props.onStartGame(selectedNumber)}/>
+        <MainButton onPress={() => props.onStartGame(selectedNumber)}>START GAME</MainButton>
       </Card>
     );
   }
