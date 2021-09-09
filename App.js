07be0d9bc7ef58a7bf5687ad420de60707fd0dae
TriCodeIT <bayu165kusuma@gmail.com>
2021-09-09 11:10:16 +0700

    Using the SafeAreaView

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
+import React, { useState } from "react";
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
-
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen';
-import GameOverScreen from './screens/GameOverScreen';
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
 };
 
@@ -36,11 +35,11 @@ export default function App() {
     setUserNumber(null);
   };
 
-  const startGameHandler = selectedNumber => {
+  const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
   };
 
-  const gameOverHandler = numOfRounds => {
+  const gameOverHandler = (numOfRounds) => {
     setGuessRounds(numOfRounds);
   };
 
@@ -68,15 +67,15 @@ export default function App() {
   }
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title="Guess a Number" />
       {content}
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -17,30 +17,30 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = (props) => {
   return (
-    <ScrollView>
-      <View style={styles.screen}>
-        <TitleText>The Game is Over</TitleText>
-        <View style={styles.imageContainer}>
-          <Image
-            // source={require("../assets/success.png")}
-            source={{
-              uri: "https://static01.nyt.com/images/2021/01/20/sports/19ALTsummit-k2-2-print/19summit-k2-2-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-            }}
-            style={styles.image}
-            resizeMode="cover"
-          />
+      <ScrollView>
+        <View style={styles.screen}>
+          <TitleText>The Game is Over</TitleText>
+          <View style={styles.imageContainer}>
+            <Image
+              // source={require("../assets/success.png")}
+              source={{
+                uri: "https://static01.nyt.com/images/2021/01/20/sports/19ALTsummit-k2-2-print/19summit-k2-2-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+              }}
+              style={styles.image}
+              resizeMode="cover"
+            />
+          </View>
+          <View style={styles.resultContainer}>
+            <BodyText style={styles.resultText}>
+              Your phone needed{" "}
+              <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds
+              to guess the number{" "}
+              <Text style={styles.highlight}>{props.userNumber}</Text>.
+            </BodyText>
+          </View>
+          <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
         </View>
-        <View style={styles.resultContainer}>
-          <BodyText style={styles.resultText}>
-            Your phone needed{" "}
-            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-            guess the number{" "}
-            <Text style={styles.highlight}>{props.userNumber}</Text>.
-          </BodyText>
-        </View>
-        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-      </View>
-    </ScrollView>
+      </ScrollView>
   );
 };
 
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingVertical: 10
+    paddingVertical: 10,
   },
   imageContainer: {
     width: Dimensions.get("window").width * 0.7,
