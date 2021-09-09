db3e6bfb84c89f4245d5c1570fb4c97959095782
TriCodeIT <bayu165kusuma@gmail.com>
2021-09-09 10:23:08 +0700

    Working with Platform.select() and Platform in if Checks

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import {View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, Platform } from "react-native";
 
-import Colors from '../constants/colors'
-import TitleText from './TitleText';
+import Colors from "../constants/colors";
+import TitleText from "./TitleText";
 
-const Header = props => {
-    return (
-        <View style={styles.header}>
-            <TitleText>{props.title}</TitleText>
-        </View>
-    )
-
-}
+const Header = (props) => {
+  return (
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
+      <TitleText style={styles.title}>{props.title}</TitleText>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    header : {
-        width: '100%',
-        height: 90,
-        paddingTop: 36,
-        backgroundColor: Colors.primary,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  headerBase: {
+    width: "100%",
+    height: 90,
+    paddingTop: 36,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerIOS: {
+    backgroundColor: "white",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
+  },
+  title: {
+    color: Platform.OS === "ios" ? Colors.primary : "white",
+  },
+});
 
-export default Header
+export default Header;
